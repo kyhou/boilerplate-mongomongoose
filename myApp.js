70dfbd231f4b5ea0442fcfd8673d0e46837535e4
Kyhou <joelskill@gmail.com>
2022-10-30 22:15:58 -0300

    Perform Updates by Running Find, Edit, then Save

--- myApp.js
+++ myApp.js
@@ -59,7 +59,14 @@ const findPersonById = (personId, done) => {
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, function (err, reg) {
+    if (err) return console.error(err);
+    reg.favoriteFoods.push(foodToAdd);
+    Person.save(reg, function (err, data) {
+      if (err) return console.error(err);
+      done(null, data);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
